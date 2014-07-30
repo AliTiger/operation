f3c039b5ff032bc4b33202dbd29084f04aaa5599
hautlixiaodong <hautlixiaodong@163.com>
2014-07-30 15:30:29 +0800

    1:修改用户活跃页面模板 2:修改用户在线页面模板 3:修改用户留存页面模板

--- routes/activeUsers.js
+++ routes/activeUsers.js
@@ -9,7 +9,7 @@ exports.get = function(req, res){
 	params.title = '活跃用户数量';
 	params.user = req.session.user;
 	params.users = req.session.users;
-	if(page_id == '2-1'){
+	if(page_id == '2-1' || page_id == '2-5'){
 		active  = [90000,90000,98000,90000,97000,99000,90000,90000,98000,92000,97000,99000,90000,90000,98000,90000,97000,99000,90000,97000,98000,92000,97000,99000,92000,93000,96000,91000,94000,94000,92000];
 		params.active = active;
 
--- routes/remainUsers.js
+++ routes/remainUsers.js
@@ -2,7 +2,15 @@
 
 exports.get = function(req, res){
 	var page_id = req.params.page_id;
+	var active_user = [45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,45,52,54,74,90,84,54,74,90,84];
+	var high_active_user =  [60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,60,80,105,125,108,120,105,125,108,120];
+	var params = {};
+	params.title = '回流用户数量';
+	params.user = req.session.user;
+	params.users = req.session.users;
+	params.active_user = active_user;
+	params.high_active_user = high_active_user;
 
 	console.log('====page_id',page_id);
-	return res.render(page_id+'.ejs',{title:'获取用户',user:req.session.user,users:req.session.users});
+	return res.render(page_id+'.ejs',params);
 }
