0d9dfa42bacff5978d7b71f17b5835765f7a7704
李晓东 <hautlixiaodong@gmail.com>
2014-07-30 14:36:32 +0800

    Update mongo-command.js

--- mongo-command.js
+++ mongo-command.js
@@ -1,7 +1,7 @@
 
 //添加用户
-db.users.insert({_id:1,name:'lixiaodong',password:'123456',power:[2,4,8,16,32,64,128,256]});
-db.users.insert({_id:2,name:'wangzhao',password:'123456',power:[2,4,8,16,32,126]});
+db.users.insert({_id:1,name:'lixiaodong',password:'123456',power:[1,2,4,8,16,32,64,128,256]});
+db.users.insert({_id:2,name:'wangzhao',password:'123456',power:[1,2,4,8,16,32,126]});
 db.users.insert({_id:3,name:'zhaoge',password:'123456',power:[8,16,256]});
 
 
