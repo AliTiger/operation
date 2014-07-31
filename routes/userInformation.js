0f1aa395252699760ecd07384a751226e36bd9f1
hautlixiaodong <hautlixiaodong@163.com>
2014-07-31 18:30:44 +0800

    调整登陆页面背景图片

--- routes/userInformation.js
+++ routes/userInformation.js
@@ -9,8 +9,6 @@ exports.get = function(req, res){
 	params.title = '用户信息';
 	params.user = req.session.user;
 	params.users = req.session.users;
-	params.active_user = active_user;
-	params.high_active_user = high_active_user;
 	
 	console.log('====page_id',page_id);
 	return res.render(page_id+'.ejs',params);
